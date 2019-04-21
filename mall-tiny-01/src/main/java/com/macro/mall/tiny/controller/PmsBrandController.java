a1777621295e763242b09ed9517031a8c9b9f286
macro <zhenghong1991@126.com>
2019-04-21 12:03:44 +0800

    基本项目骨架搭建

---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
@@ -0,0 +1,91 @@
+package com.macro.mall.tiny.controller;
+
+import com.macro.mall.tiny.common.api.CommonPage;
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.mbg.model.PmsBrand;
+import com.macro.mall.tiny.service.PmsBrandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+/**
+ * 品牌管理Controller
+ * Created by macro on 2019/4/19.
+ */
+@Controller
+@RequestMapping("/brand")
+public class PmsBrandController {
+    @Autowired
+    private PmsBrandService demoService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
+
+    @RequestMapping(value = "listAll", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<PmsBrand>> getBrandList() {
+        return CommonResult.success(demoService.listAllBrand());
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult createBrand(@RequestBody PmsBrand pmsBrand) {
+        CommonResult commonResult;
+        int count = demoService.createBrand(pmsBrand);
+        if (count == 1) {
+            commonResult = CommonResult.success(pmsBrand);
+            LOGGER.debug("createBrand success:{}", pmsBrand);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            LOGGER.debug("createBrand failed:{}", pmsBrand);
+        }
+        return commonResult;
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand pmsBrandDto, BindingResult result) {
+        CommonResult commonResult;
+        int count = demoService.updateBrand(id, pmsBrandDto);
+        if (count == 1) {
+            commonResult = CommonResult.success(pmsBrandDto);
+            LOGGER.debug("updateBrand success:{}", pmsBrandDto);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            LOGGER.debug("updateBrand failed:{}", pmsBrandDto);
+        }
+        return commonResult;
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult deleteBrand(@PathVariable("id") Long id) {
+        int count = demoService.deleteBrand(id);
+        if (count == 1) {
+            LOGGER.debug("deleteBrand success :id={}", id);
+            return CommonResult.success(null);
+        } else {
+            LOGGER.debug("deleteBrand failed :id={}", id);
+            return CommonResult.failed("操作失败");
+        }
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                                        @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize) {
+        List<PmsBrand> brandList = demoService.listBrand(pageNum, pageSize);
+        return CommonResult.success(CommonPage.restPage(brandList));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<PmsBrand> brand(@PathVariable("id") Long id) {
+        return CommonResult.success(demoService.getBrand(id));
+    }
+}
---
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/mbg/model/PmsBrand.java
@@ -0,0 +1,174 @@
+package com.macro.mall.tiny.mbg.model;
+
+import java.io.Serializable;
+
+public class PmsBrand implements Serializable {
+    private Long id;
+
+    private String name;
+
+    /**
+     * 首字母
+     *
+     * @mbggenerated
+     */
+    private String firstLetter;
+
+    private Integer sort;
+
+    /**
+     * 是否为品牌制造商：0->不是；1->是
+     *
+     * @mbggenerated
+     */
+    private Integer factoryStatus;
+
+    private Integer showStatus;
+
+    /**
+     * 产品数量
+     *
+     * @mbggenerated
+     */
+    private Integer productCount;
+
+    /**
+     * 产品评论数量
+     *
+     * @mbggenerated
+     */
+    private Integer productCommentCount;
+
+    /**
+     * 品牌logo
+     *
+     * @mbggenerated
+     */
+    private String logo;
+
+    /**
+     * 专区大图
+     *
+     * @mbggenerated
+     */
+    private String bigPic;
+
+    /**
+     * 品牌故事
+     *
+     * @mbggenerated
+     */
+    private String brandStory;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstLetter() {
+        return firstLetter;
+    }
+
+    public void setFirstLetter(String firstLetter) {
+        this.firstLetter = firstLetter;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public Integer getFactoryStatus() {
+        return factoryStatus;
+    }
+
+    public void setFactoryStatus(Integer factoryStatus) {
+        this.factoryStatus = factoryStatus;
+    }
+
+    public Integer getShowStatus() {
+        return showStatus;
+    }
+
+    public void setShowStatus(Integer showStatus) {
+        this.showStatus = showStatus;
+    }
+
+    public Integer getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(Integer productCount) {
+        this.productCount = productCount;
+    }
+
+    public Integer getProductCommentCount() {
+        return productCommentCount;
+    }
+
+    public void setProductCommentCount(Integer productCommentCount) {
+        this.productCommentCount = productCommentCount;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getBigPic() {
+        return bigPic;
+    }
+
+    public void setBigPic(String bigPic) {
+        this.bigPic = bigPic;
+    }
+
+    public String getBrandStory() {
+        return brandStory;
+    }
+
+    public void setBrandStory(String brandStory) {
+        this.brandStory = brandStory;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", firstLetter=").append(firstLetter);
+        sb.append(", sort=").append(sort);
+        sb.append(", factoryStatus=").append(factoryStatus);
+        sb.append(", showStatus=").append(showStatus);
+        sb.append(", productCount=").append(productCount);
+        sb.append(", productCommentCount=").append(productCommentCount);
+        sb.append(", logo=").append(logo);
+        sb.append(", bigPic=").append(bigPic);
+        sb.append(", brandStory=").append(brandStory);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
