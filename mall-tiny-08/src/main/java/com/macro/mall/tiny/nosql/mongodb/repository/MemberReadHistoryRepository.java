a0722980bd530622171cc59fe1c977047f648c21
macro <zhenghong1991@126.com>
2019-05-18 14:26:11 +0800

    add mall-tiny-08

---
+++ mall-tiny-08/src/main/java/com/macro/mall/tiny/nosql/mongodb/repository/MemberReadHistoryRepository.java
@@ -0,0 +1,19 @@
+package com.macro.mall.tiny.nosql.mongodb.repository;
+
+
+import com.macro.mall.tiny.nosql.mongodb.document.MemberReadHistory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * 会员商品浏览历史Repository
+ * Created by macro on 2018/8/3.
+ */
+public interface MemberReadHistoryRepository extends MongoRepository<MemberReadHistory,String> {
+    /**
+     * 根据会员id按时间倒序获取浏览记录
+     * @param memberId 会员id
+     */
+    List<MemberReadHistory> findByMemberIdOrderByCreateTimeDesc(Long memberId);
+}
