a58911c0270681c0103e0be8f2363fde8b67dd24
macro <zhenghong1991@126.com>
2019-06-08 18:31:07 +0800

    add mall-tiny-docker

---
+++ mall-tiny-docker/src/main/java/com/macro/mall/tiny/MallTiny01Application.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTiny01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTiny01Application.class, args);
+    }
+
+}
