22f7baee6e504a165cda862a3d9d6fdd444428cf
macro <zhenghong1991@126.com>
2019-09-01 15:36:01 +0800

    add mall-tiny-stream

---
+++ mall-tiny-stream/src/main/java/com/macro/mall/tiny/mbg/mapper/UmsPermissionMapper.java
@@ -0,0 +1,30 @@
+package com.macro.mall.tiny.mbg.mapper;
+
+import com.macro.mall.tiny.mbg.model.UmsPermission;
+import com.macro.mall.tiny.mbg.model.UmsPermissionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsPermissionMapper {
+    long countByExample(UmsPermissionExample example);
+
+    int deleteByExample(UmsPermissionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsPermission record);
+
+    int insertSelective(UmsPermission record);
+
+    List<UmsPermission> selectByExample(UmsPermissionExample example);
+
+    UmsPermission selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByExample(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByPrimaryKeySelective(UmsPermission record);
+
+    int updateByPrimaryKey(UmsPermission record);
+}
