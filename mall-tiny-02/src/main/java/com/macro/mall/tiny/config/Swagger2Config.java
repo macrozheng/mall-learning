00f26e234a2da71a150923e70ba9104697485b0f
hjd <hjd1622@qq.com>
2019-12-12 15:06:21 +0800

    补充整理-tiny-02(Swagger2案例)

--- mall-tiny-02/src/main/java/com/macro/mall/tiny/config/Swagger2Config.java
+++ mall-tiny-02/src/main/java/com/macro/mall/tiny/config/Swagger2Config.java
@@ -1,5 +1,7 @@
 package com.macro.mall.tiny.config;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -12,6 +14,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger2API文档的配置
+ *
+ * 1,@Api：用于修饰Controller类，生成Controller相关文档信息
+ * 2,@ApiOperation：用于修饰Controller类中的方法，生成接口方法相关文档信息
+ * 3,@ApiParam：用于修饰接口中的参数，生成接口参数相关文档信息
+ * 4,@ApiModelProperty：用于修饰实体类的属性，当实体类是请求参数或返回结果时，直接生成相关文档信息
+ * 5,@ApiImplicitParam ：用在@ApiImplicitParams注解中，指定一个请求参数的各个方面
+ *     paramType：参数放在哪个地方
+ *         header-->请求参数的获取：@RequestHeader
+ *         query-->请求参数的获取：@RequestParam
+ *         path（用于restful接口）-->请求参数的获取：@PathVariable
+ *         body（不常用）
+ *         form（不常用）
+ *     name：参数名
+ *     dataType：参数类型
+ *     required：参数是否必须传
+ *     value：参数的意思
+ *     defaultValue：参数的默认值
+ * 6,@ApiResponses ：用于表示一组响应
+ * 7,@ApiResponse ：用在@ApiResponses中，一般用于表达一个错误的响应信息
+ *     code：数字，例如400
+ *     message：信息，例如"请求参数没填好"
+ *     response：抛出异常的类
  */
 @Configuration
 @EnableSwagger2
@@ -23,7 +47,7 @@ public class Swagger2Config {
                 .select()
                 //为当前包下controller生成API文档
                 .apis(RequestHandlerSelectors.basePackage("com.macro.mall.tiny.controller"))
-                //为有@Api注解的Controller生成API文档
+//                为有@Api注解的Controller生成API文档
 //                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 //为有@ApiOperation注解的方法生成API文档
 //                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
--- mall-tiny-02/src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
+++ mall-tiny-02/src/main/java/com/macro/mall/tiny/controller/PmsBrandController.java
@@ -4,9 +4,7 @@ import com.macro.mall.tiny.common.api.CommonPage;
 import com.macro.mall.tiny.common.api.CommonResult;
 import com.macro.mall.tiny.mbg.model.PmsBrand;
 import com.macro.mall.tiny.service.PmsBrandService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +19,7 @@ import java.util.List;
  * 品牌管理Controller
  * Created by macro on 2019/4/19.
  */
-@Api(tags = "PmsBrandController", description = "商品品牌管理")
+@Api(tags = "PmsBrandController", description = "商品品牌管理111")
 @Controller
 @RequestMapping("/brand")
 public class PmsBrandController {
@@ -40,7 +38,7 @@ public class PmsBrandController {
     @ApiOperation("添加品牌")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     @ResponseBody
-    public CommonResult createBrand(@RequestBody PmsBrand pmsBrand) {
+    public CommonResult createBrand( @ApiParam("新增的记录") @RequestBody PmsBrand pmsBrand) {
         CommonResult commonResult;
         int count = brandService.createBrand(pmsBrand);
         if (count == 1) {
@@ -54,6 +52,15 @@ public class PmsBrandController {
     }
 
     @ApiOperation("更新指定id品牌信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "PmsBrand",paramType = "create",value = "修改的属性信息",required = true),
+            @ApiImplicitParam(name = "BindingResult",paramType = "base",value = "绑定的数据",required = false)
+    })
+    @ApiResponses({
+            @ApiResponse(code = 401,message = "你没权限"),
+            @ApiResponse(code = 403,message = "你被禁止访问了"),
+            @ApiResponse(code = 404,message = "没找到，哈哈哈")
+    })
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand pmsBrandDto, BindingResult result) {
