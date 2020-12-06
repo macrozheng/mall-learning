358e2cbd71d37aa9359d61484453970d27215d01
macro <zhenghong1991@126.com>
2020-12-06 10:53:58 +0800

    add mall-tiny-docker-plugin

---
+++ mall-tiny-docker-plugin/src/test/java/com/macro/mall/tiny/MallTinyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.macro.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallTinyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
