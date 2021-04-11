2030296bc3fd3717c461944bb629a2ed582c4ae9
macro <zhenghong1991@126.com>
2021-04-11 20:51:00 +0800

    add mall-tiny-devtools

---
+++ mall-tiny-devtools/src/main/java/com/macro/mall/tiny/service/PmsBrandService.java
@@ -0,0 +1,24 @@
+package com.macro.mall.tiny.service;
+
+
+import com.macro.mall.tiny.mbg.model.PmsBrand;
+
+import java.util.List;
+
+/**
+ * PmsBrandService
+ * Created by macro on 2019/4/19.
+ */
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
