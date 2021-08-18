c5f26a87a8777d568e76ceceec180014dcf8bffb
macro <zhenghong1991@126.com>
2021-08-18 20:45:37 +0800

    add mall-tiny-sa-token

---
+++ mall-tiny-sa-token/src/main/java/com/macro/mall/tiny/controller/UmsAdminController.java
@@ -0,0 +1,51 @@
+package com.macro.mall.tiny.controller;
+
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.service.UmsAdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 后台用户管理
+ * Created by macro on 2018/4/26.
+ */
+@Controller
+@Api(tags = "UmsAdminController", description = "后台用户管理")
+@RequestMapping("/admin")
+public class UmsAdminController {
+    @Autowired
+    private UmsAdminService adminService;
+
+    @ApiOperation(value = "登录以后返回token")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult login(@RequestParam String username, @RequestParam String password) {
+        SaTokenInfo saTokenInfo = adminService.login(username, password);
+        if (saTokenInfo == null) {
+            return CommonResult.validateFailed("用户名或密码错误");
+        }
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", saTokenInfo.getTokenValue());
+        tokenMap.put("tokenHead", saTokenInfo.getTokenName());
+        return CommonResult.success(tokenMap);
+    }
+
+    @ApiOperation(value = "查询当前登录状态")
+    @RequestMapping(value = "/isLogin",method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult isLogin() {
+        return CommonResult.success(StpUtil.isLogin());
+    }
+}
