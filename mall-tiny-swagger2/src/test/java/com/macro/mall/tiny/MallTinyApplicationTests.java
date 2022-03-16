c71f12e9a75f1fe302fe0771046d9f8bc9ff56c5
macro <zhenghong1991@126.com>
2022-03-16 15:13:45 +0800

    add mall-tiny-swagger2

---
+++ mall-tiny-swagger2/src/test/java/com/macro/mall/tiny/MallTinyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class MallTinyApplicationTests {
+
+    public void contextLoads() {
+    }
+
+}
